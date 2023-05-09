2931fbb3a1a77fb5b7771ea8138c04c3c22722ea
Jannik <8103530+wh1zk1d@users.noreply.github.com>
2023-05-09 23:19:37 +0200

    clean up ui

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -7,13 +7,16 @@ export function Header() {
   const user = useOptionalUser()
 
   return (
-    <header className="flex items-center justify-between bg-white p-6 shadow-sm">
+    <header className="flex h-20 items-center justify-between bg-white px-6 shadow-sm">
       <Link to="/" className="font-semibold">
         {config.appName}
       </Link>
       {user ? (
         <div className="flex items-center space-x-4 text-sm">
-          <Link to="/profile" className="inline-flex items-center">
+          <Link
+            to="/profile"
+            className="inline-flex items-center rounded-md bg-transparent p-2 transition-colors hover:bg-slate-50"
+          >
             <UserCircleIcon className="mr-1.5 h-4 w-4" /> {user.email}
           </Link>
         </div>
