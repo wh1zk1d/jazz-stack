554234adc86409cdeb727acbd380f0a9b01dd822
Jannik <8103530+wh1zk1d@users.noreply.github.com>
2023-05-10 13:19:31 +0200

    make index route protected

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,38 +1,27 @@
+import type { LoaderArgs} from "@remix-run/node";
+import { json } from "@remix-run/node"
 import type { V2_MetaFunction } from "@remix-run/react"
-import { Link } from "@remix-run/react"
-import { twMerge } from "tailwind-merge"
 import { Card, Container, PageTitle } from "~/components/Primitives"
-import { buttonClasses } from "~/components/Primitives/Button"
-import { config } from "~/config"
-import { useOptionalUser } from "~/utils/misc"
+import { requireUserId } from "~/utils/session.server"
 
 export const meta: V2_MetaFunction = () => {
-  return [{ title: config.appName }]
+  return [{ title: 'Dashboard' }]
+}
+
+export async function loader({ request }: LoaderArgs) {
+  await requireUserId(request)
+  return json({})
 }
 
 export default function Index() {
-  const user = useOptionalUser()
-
   return (
     <Container className="pt-14">
-      <PageTitle>{user ? "Dashboard" : "Hello, friend!"}</PageTitle>
+      <PageTitle>Dashboard</PageTitle>
       <Card className="flex flex-col items-start gap-2">
         <p className="font-semibold">Welcome to your new Remix App!</p>
         <p>
           You are now using the Jazz Stack. Go build something great with it.
         </p>
-        {!user && (
-          <Link
-            to="/login"
-            className={twMerge(
-              buttonClasses.base,
-              buttonClasses.variants.light,
-              "mt-2"
-            )}
-          >
-            Login
-          </Link>
-        )}
       </Card>
     </Container>
   )
